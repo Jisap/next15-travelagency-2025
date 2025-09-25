9fffc27468e3427a9f3cffdc1a2660f6f65d49b7
Jorge Matos <jisapjisap@gmail.com>
2025-09-25 13:41:16 +0200

    Add additional content to ExperienceComponent, including a descriptive paragraph and a "Learn More" button to enhance user engagement and provide travelers with more information about curated experiences.

--- components/ExperienceComponent.jsx
+++ components/ExperienceComponent.jsx
@@ -7,6 +7,18 @@ const ExperienceComponent = () => {
         <h2 className='xl:w-[50%] w-full text-white text-2xl md:text-5xl leading-tight unbounded-font'>
           Enjoy Our best Quality Tour & Experience
         </h2>
+
+        <div className='xl:w-[40%] w-full'>
+          <p className='text-[#ffffff91] pb-5 text-sm'>
+            Our curated travel experiences are designed to inspire and delight. But don't just take our word for it see what our happy travelers have to say
+          </p>
+          
+          <button className='btn bg-white group text-[#193555] hover:bg-[#193555]font-bold px-6 w-auto'>
+            <a href="#" className='unbounded-font text-sm xl:text-md uppercase group-hover:text-white'>
+              Learn More
+            </a>
+          </button>
+        </div>
       </div>
     </div>
   )
